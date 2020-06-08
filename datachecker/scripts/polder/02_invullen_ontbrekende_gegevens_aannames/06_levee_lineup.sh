d1a47e441bb84976e24532aecfc444991e23b481
emielverstegen <emiel@verstegen.email>
2020-06-08 15:37:58 +0200

    Remove reference to 2x2m DEM, use 0.5x0.5m in line-up

--- datachecker/scripts/polder/02_invullen_ontbrekende_gegevens_aannames/06_levee_lineup.sh
+++ datachecker/scripts/polder/02_invullen_ontbrekende_gegevens_aannames/06_levee_lineup.sh
@@ -4,6 +4,6 @@ mkdir /code/data/line-up
 echo "Exporting levees for line-up tool"
 ogr2ogr -overwrite -sql "SELECT * FROM tmp.levee_split" -f "ESRI Shapefile" /code/data/line-up/levee_split.shp PG:"host=db user=postgres dbname=datachecker password=postgres port=5432"
 echo "Executing line-up tool"
-line-up /code/data/fixed_data/ahn3_raster/hhnk_ahn3_2x2.tif /code/data/line-up/levee_split.shp /code/data/line-up/levee_height.shp -d 6 -w 2 -a 75 -l line -f lineupid -e height
+line-up /code/data/fixed_data/DEM/dem.vrt /code/data/line-up/levee_split.shp /code/data/line-up/levee_height.shp -d 6 -w 2 -a 75 -l line -f lineupid -e height
 echo "Importing line-up tool result"
 ogr2ogr -f "PostgreSQL" PG:"host=db user=postgres dbname=datachecker password=postgres port=5432" -lco "SCHEMA=tmp" -lco "FID=objectid" -nlt LINESTRING -lco "OVERWRITE=YES" -a_srs EPSG:28992 -progress -overwrite /code/data/line-up/levee_height.shp
