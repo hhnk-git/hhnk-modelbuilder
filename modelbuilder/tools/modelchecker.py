2d8d6e0abb540a270a69906a910130d8ce7f3236
Ckerklaan <38913567+Ckerklaan@users.noreply.github.com>
2020-06-17 16:20:39 +0200

    Added absolute path in bash file instead of python script.

--- modelbuilder/tools/modelchecker.py
+++ modelbuilder/tools/modelchecker.py
@@ -22,19 +22,19 @@ def get_parser():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
-        'filename', default='name',
-        help='filename (without path)')
+        'sqlite_path', default='name.sqlite',
+        help='sqlite_path (sqlite with path)')
     return parser
 
 def run_modelchecker(**kwargs):
     print('Starting modelchecker')
-    name = kwargs.get('filename')
-    logging.basicConfig(filename=f'/code/data/output/{name}_modelchecker.txt',level=logging.DEBUG)
+    sqlite_path = kwargs.get('sqlite_path')
+    log_path = sqlite_path.split(".")[0] + "_modelchecker.txt"
+    logging.basicConfig(filename=log_path,level=logging.DEBUG)
 
 
-    sqlite_file = f"/code/data/output/{name}.sqlite"
     database = ThreediDatabase(
-        connection_settings={"db_path": sqlite_file}, db_type="spatialite"
+        connection_settings={"db_path": sqlite_path}, db_type="spatialite"
     )
     model_checker = ThreediModelChecker(database)
     for check, error in model_checker.errors():
