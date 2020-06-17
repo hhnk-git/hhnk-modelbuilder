fff12f08b78261cde83ae741bc4df7fb44bb58fc
emielverstegen <emiel@verstegen.email>
2020-06-17 17:26:25 +0200

    Removed non-UTF8 characters from SQL scripts, removed pwd from SH, edited line-endings sh scripts

--- modelbuilder/scripts/polder/04c_snapping_clipping_circular.sh
+++ modelbuilder/scripts/polder/04c_snapping_clipping_circular.sh
@@ -5,7 +5,6 @@
 # clip circular knipt watergangen met het start en eindpunt op elkaar in tweeen (mag niet in 3Di)
 ##
 #!/bin/bash
-pwd
 python3 /code/modelbuilder/tools/threedi-base/manage.py 02_fix_channels /code/modelbuilder/tools/threedi-base/config.ini
 python3 /code/modelbuilder/tools/threedi-base/manage.py 03_clip_channel_by_culvert --search-radius 0.1 /code/modelbuilder/tools/threedi-base/config.ini
 python3 /code/modelbuilder/tools/threedi-base/manage.py 04_cut_circular /code/modelbuilder/tools/threedi-base/config.ini
