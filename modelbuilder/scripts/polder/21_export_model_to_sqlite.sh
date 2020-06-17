e3c6eeafd9a535d92a15d078442ace849e3bba4c
Ckerklaan <38913567+Ckerklaan@users.noreply.github.com>
2020-06-17 15:34:08 +0200

    Added modelchecker

--- modelbuilder/scripts/polder/21_export_model_to_sqlite.sh
+++ modelbuilder/scripts/polder/21_export_model_to_sqlite.sh
@@ -2,3 +2,4 @@
 #rm ./models/* -r
 #/srv/modelbuilder/prd/threedi-turtle/bin/django export_work_to_spatialite work_modelbuilder /srv/bwn_modelbuilder/models/bwn_$2.sqlite
 python3 /code/modelbuilder/tools/threedi-export/export_threedi.py /code/data/output/bwn_$2.sqlite
+python3 /code/modelbuilder/tools/modelchecker.py bwn_$2
