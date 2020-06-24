e03c28851d95fb8a8b9123d86c662ba6e5a1196a
Ckerklaan <38913567+Ckerklaan@users.noreply.github.com>
2020-06-24 11:33:11 +0200

    Updated the output of the modelchecker.

--- modelbuilder/tools/modelchecker.py
+++ modelbuilder/tools/modelchecker.py
@@ -5,12 +5,14 @@ Created on Wed Jun 17 14:51:04 2020
 @author: chris.kerklaan
 """
 
+import os
 import sys
+import csv
 import logging
 import argparse
 
 
-from threedi_modelchecker.exporters import format_check_results
+from threedi_modelchecker.exporters import format_check_results, export_to_file
 from threedi_modelchecker.model_checks import ThreediModelChecker
 from threedi_modelchecker.threedi_database import ThreediDatabase
 
@@ -29,16 +31,36 @@ def get_parser():
 def run_modelchecker(**kwargs):
     print('Starting modelchecker')
     sqlite_path = kwargs.get('sqlite_path')
-    log_path = sqlite_path.split(".")[0] + "_modelchecker.txt"
-    logging.basicConfig(filename=log_path,level=logging.DEBUG)
-
+    log_path = sqlite_path.split(".")[0] + "_modelchecker.csv"
+    if os.path.exists(log_path):
+        os.remove(log_path)
+                
 
     database = ThreediDatabase(
         connection_settings={"db_path": sqlite_path}, db_type="spatialite"
     )
     model_checker = ThreediModelChecker(database)
-    for check, error in model_checker.errors():
-        logging.info(format_check_results(check, error))
+    session = model_checker.db.get_session()
+    with open(log_path, "w", newline="") as output_file:
+        writer = csv.writer(output_file)
+        writer.writerow(["id", "table", "column", "value", "description", "type of check"])
+        for i, check in enumerate(model_checker.checks()):
+            model_errors = check.get_invalid(session)
+            for error_row in model_errors:
+                writer.writerow(
+                    [
+                        error_row.id,
+                        check.table.name,
+                        check.column.name,
+                        getattr(error_row, check.column.name),
+                        check.description(),
+                        check,
+                    ]
+                )
+    #export_to_file(model_checker.errors(), log_path)
+    
+    #for check, error in model_checker.errors():
+    #   logging.info(format_check_results(check, error))
 
 def main():
     try:
