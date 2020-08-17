1945f25de416f4b2d253c541de6a219462ddb494
wvanesse <48908991+wvanesse@users.noreply.github.com>
2020-08-17 09:20:30 +0200

    formatting update

--- modelbuilder/scripts/polder/09_aanvullen_netwerk.sql
+++ modelbuilder/scripts/polder/09_aanvullen_netwerk.sql
@@ -1,27 +1,70 @@
 /*
 Vul het netwerk, gemaakt in de vorige stap, aan met de orginele codes van de kunstwerken.
 */
-
 --TODO: moeten deze selectie naar een check tabel?
-SELECT * FROM deelgebied.tmp_sel_culvert WHERE connection_node_end_id IS NULL OR connection_node_start_id IS NULL;
--- 12-04-2017: 0 hits voor de purmer (dus damo is nice en/of script Lars werkt goed!)
-ALTER TABLE deelgebied.tmp_sel_culvert ADD COLUMN code varchar(50);
-UPDATE deelgebied.tmp_sel_culvert as a
-SET code = b.code
-FROM deelgebied.culvert as b
-WHERE a.culvert_id = b.id
+SELECT *
+FROM
+       deelgebied.tmp_sel_culvert
+WHERE
+       connection_node_end_id      IS NULL
+       OR connection_node_start_id IS NULL
 ;
+
+-- 12-04-2017: 0 hits voor de purmer (dus damo is nice en/of script Lars werkt goed!)
+ALTER TABLE deelgebied.tmp_sel_culvert ADD COLUMN code varchar(50)
+;
+
+UPDATE
+       deelgebied.tmp_sel_culvert as a
+SET    code = b.code
+FROM
+       deelgebied.culvert as b
+WHERE
+       a.culvert_id = b.id
+;
+
 -- kan eigenlijk niet gebeuren voor stuwen en gemalen
-SELECT * FROM deelgebied.tmp_sel_weirs WHERE connection_node_end_id IS NULL OR connection_node_start_id IS NULL;
-SELECT * FROM deelgebied.tmp_sel_pumpstation WHERE connection_node_end_id IS NULL OR connection_node_start_id IS NULL;
+SELECT *
+FROM
+       deelgebied.tmp_sel_weirs
+WHERE
+       connection_node_end_id      IS NULL
+       OR connection_node_start_id IS NULL
+;
+
+SELECT *
+FROM
+       deelgebied.tmp_sel_pumpstation
+WHERE
+       connection_node_end_id      IS NULL
+       OR connection_node_start_id IS NULL
+;
+
 -- TODO: renier: "als ik het goed begrijp gooien we nu een multipart duiker helemaal weg. Wellicht singleparts van maken en dan 1 overhouden??"
 -- duikers multipart of duikers met dubbele id's
-DELETE FROM deelgebied.tmp_sel_culvert
-WHERE culvert_id IN (
-WITH tel AS (
-	SELECT count(*), culvert_id, code
-	FROM deelgebied.tmp_sel_culvert
-	GROUP BY culvert_id, code
-	)
-SELECT culvert_id FROM tel WHERE count > 1
-);
+DELETE
+FROM
+       deelgebied.tmp_sel_culvert
+WHERE
+       culvert_id IN
+                     (
+                          WITH tel AS
+                               (
+                                        SELECT
+                                                 count(*)
+                                               , culvert_id
+                                               , code
+                                        FROM
+                                                 deelgebied.tmp_sel_culvert
+                                        GROUP BY
+                                                 culvert_id
+                                               , code
+                               )
+                          SELECT
+                                 culvert_id
+                          FROM
+                                 tel
+                          WHERE
+                                 count > 1
+       )
+;
