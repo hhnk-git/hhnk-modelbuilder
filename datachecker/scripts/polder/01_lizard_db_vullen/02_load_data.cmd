a4e90e60834785987918f2c31c9959dde076c81a
Wouter van Esse <48908991+wvanesse@users.noreply.github.com>
2025-11-28 13:32:26 +0100

    clean up before start

--- datachecker/scripts/polder/01_lizard_db_vullen/02_load_data.cmd
+++ datachecker/scripts/polder/01_lizard_db_vullen/02_load_data.cmd
@@ -1,5 +1,10 @@
 set "DATA_ROOT=%~1"
 
+echo clean up existing files
+rmdir /s /q "%DATA_ROOT%\tmp"
+rmdir /s /q "%DATA_ROOT%\line-up"
+rmdir /s /q "%DATA_ROOT%\output"
+
 echo "Loading DAMO data @ %DATA_ROOT%\input\DAMO.gpkg
 ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=datachecker password=postgres port=5433" -nlt CONVERT_TO_LINEAR -lco "GEOMETRY_NAME=wkb_geometry" -lco "SCHEMA=damo_ruw" -lco "FID=id" -lco "OVERWRITE=YES" "%DATA_ROOT%\input\DAMO.gpkg" --config PG_USE_COPY YES -gt 1000
 echo "Loading HDB data @ \\corp.hhnk.nl\data\Hydrologen_data\Data\01.basisgegevens\00.HDB\Hydro_database.gpkg
